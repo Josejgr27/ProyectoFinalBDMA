ddc9c7419cbba1973dea59266b94db267928b8a5
Desarrollo4 <Desarrollo4@gpocsi.mx.local>
2024-05-09 17:45:18 -0700

    prureba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,99 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { RegistrosService } from './registros.service';
+
+declare var $;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
+
 export class AppComponent {
   title = 'ProyectoFinalBecas';
+
+
+  navInicio = true;
+  navBecas = false;
+  navAlumnos = false;
+  navMaterias = false;
+
+  lEstudiantes = [];
+  lBecas = [];
+  lMaterias = [];
+
+  constructor(private servProy: RegistrosService,
+    private modalService: NgbModal,
+  ) { }
+
+  clickNav(name) {
+
+    var NavArray = ['Inicio', 'Becas', 'Alumnos', 'Materias'];
+    NavArray.forEach(element => {
+      if (name == element) {
+        this['nav' + element] = true;
+        element != 'Inicio' ? this['get' + element]() : '';
+      } else {
+        this['nav' + element] = false;
+      }
+    });
+  }
+
+
+
+
+  getAlumnos() {
+    var var_busqueda = [];
+
+    this.servProy.GetEstudiantes(var_busqueda).subscribe((resp: any) => {
+      this.lEstudiantes = resp.response;
+    });
+  }
+
+  getMaterias() {
+    var var_busqueda = [];
+
+    this.servProy.GetMaterias(var_busqueda).subscribe((resp: any) => {
+      this.lMaterias = resp.response;
+
+    });
+  }
+
+  getBecas() {
+    var var_busqueda = [];
+    console.log('entro');
+
+    this.servProy.GetBecas(var_busqueda).subscribe((resp: any) => {
+      console.log(resp);
+
+      this.lBecas = resp.response;
+
+    });
+  }
+
+
+  clickVerRequisitos() {
+    $("#modalReq").modal('show');
+  }
+
+  clickVerCalificaciones() {
+    $("#modalCalif").modal('show');
+  }
+
+  clickVerBecas() {
+    $("#modalBecas").modal('show');
+  }
+
+  clickVerAlumnos() {
+    $("#modalAlum").modal('show');
+  }
 }
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +18,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgxSpinnerModule,
+    NgbModule,
+    HttpClientModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
